50807d8351825551064751d2f38760766c0cd2d7
ldiez <ldiez@tlmat.unican.es>
2024-04-08 09:34:07 +0200

    ++

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
+int main (int argc, char* argv[])
 {
-  printf ("Hello world!!!\n");
-  return -1;
+  
+
+  if (argc != 2) {
+    fprintf (stderr, "Incorrect number of parameters\n");
+    return -1;
+  }
+
+  int num = atoi(argv[1]);
+
+  fprintf (stdout, "Hello world with number %d!!!\n", num);
+
+  return 0;
 }
